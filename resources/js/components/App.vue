9e954c927cf62840607877335ca8eccb226fb4a5
Viviana Herrera Hernandez <viviana.herrera@segurosayg.com>
2022-03-31 14:04:04 -0500

    Config vue, archivo app

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-view></router-view>
+</template>
+<script>
+export default {
+    name: "App",
+    created: function () {
+    },
+    data: function () {
+        return {
+
+        }
+    },
+};
+</script>
