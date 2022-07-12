a896a8e4428a747d7ab7e433c5b58ea8eb9fd7ac
Karenyulier <karenyulierr@gmial.com>
2022-07-11 20:02:09 -0500

    ajustes diseño

--- resources/views/meet_rivera.blade.php
+++ resources/views/meet_rivera.blade.php
@@ -10,8 +10,11 @@
         <br><br>
         <p class="title_conoce"> <span class="conoce">Conoce </span> <span class="rivera">Rivera</span> </p>
         <div class="text_conoce">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tenetur. Quo neque atque quae ad dolorem quas magni aliquid delectus, harum, eos expedita voluptates perferendis ex itaque iste mollitia vero?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum illo saepe, amet non eaque debitis aut odio et. Dignissimos blanditiis delectus doloremque voluptatum nemo itaque, magni vel natus quis omnis.
+            Fue fundada en 1888 en el sector comunal "Aguas Calientes" con el nombre de San Mateo, por iniciativa de Vicente Poveda, quien construyó las primeras casas con la ayuda de los vecinos anteriormente perteneció a la familia Poveda Poveda de Italia que vendió su hacienda a la familia Rivera.
+            <br>
+            El poblado fue corregimiento de la ciudad de Neiva hasta el 17 de mayo de 1943, cuando fue segregado de los territorios de la capital para ser erigido municipio.
+            <br>
+            Fue renombrado "Rivera" en memoria a José Eustasio Rivera, escritor nacido allí en 1888.
         </div>
     </div>
     <div class="row mt-5 mb-5 segunda_parte">
@@ -30,8 +33,7 @@
                 <span class="con mr-2">con</span> <img src="img/logo.png" alt="Imagen de rivera">
             </p>
             <div class="text_con_turi">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tenetur. Quo neque atque quae ad dolorem quas magni aliquid delectus, harum, eos expedita voluptates perferendis ex itaque iste mollitia vero?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum illo saepe, amet non eaque debitis aut odio et. Dignissimos blanditiis delectus doloremque voluptatum nemo itaque, magni vel natus quis omnis.
+                Podrás ver las verdes y grandes montañas del municipio de Rivera, se ha logrado realizar el turismo de naturaleza, en donde las personas se conectan con el paisaje, la diversidad ambiental y recorren las zonas verdes del municipio, a través de los senderos ecológicos que conducen a fincas, cultivos, fuentes hídricas y demás experiencias.
             </div>
 
         </div>
